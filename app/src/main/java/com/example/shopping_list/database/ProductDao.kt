cbe8f15b698dc54013858f03ab7d7060501baa08
Tataturk <45215620+Tataturk@users.noreply.github.com>
2020-03-22 22:14:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/shopping_list/database/ProductDao.kt
@@ -0,0 +1,22 @@
+package com.example.shopping_list.database
+
+import androidx.room.*
+import com.example.shopping_list.model.Product
+
+
+@Dao
+interface ProductDao {
+
+    @Query("SELECT * FROM product_table")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query("DELETE FROM product_table")
+    suspend fun deleteAllProducts()
+
+}
